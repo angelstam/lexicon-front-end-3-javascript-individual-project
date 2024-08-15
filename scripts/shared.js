016f8c301d51866b1a34b1ef7ab6528922e90911
Johan Angelstam <johan@angelstam.se>
2024-08-15 09:17:26 +0200

    Add shared function getIdFromQueryString.

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,3 +1,9 @@
 function getFormData(form) {
     return Object.fromEntries((new FormData(form)).entries());
 }
+
+function getIdFromQueryString() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get("id");
+  }
