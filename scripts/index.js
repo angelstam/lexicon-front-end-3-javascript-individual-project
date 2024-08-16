668823fbb7902782ddfbec9e101cf9218b9762d5
Johan Angelstam <johan@angelstam.se>
2024-08-16 10:56:40 +0200

    Store the last search in cache and restore it on load.

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,18 @@
 import { getFormData } from "./shared.js";
 import * as OMDb from "./data/omdb-api.js";
-import { getFavoriteElement } from "./shared-ui.js";
+import { getFavoriteElement, getE } from "./shared-ui.js";
+import * as Cache from "./data/cache.js";
+
+const LAST_SEARCH_RESULT_CACHE_KEY = "last-search-result";
 
 window.addEventListener("load", () => {
     setupSearchListener();
+
+    if (Cache.existsInCache(LAST_SEARCH_RESULT_CACHE_KEY)) {
+        restoreSearchFromCache();
+    }
 });
+
 
 function setupSearchListener() {
     document.forms.search.addEventListener("submit", event => {
@@ -37,26 +45,50 @@ function searchListenerAction(event) {
 
 async function renderSearchResults(search) {
     const answer = await OMDb.searchOmdbMovie(search.query, search.type, search.year);
-    const searchResults = document.querySelector("#search-results");
-    const numResults = document.querySelector("#num-results");
-    searchResults.textContent = "";
-    numResults.textContent = "";
+
+    answer.SearchQuery = search;
+
+    getE("#search-results").textContent = "";
+    getE("#num-results").textContent = "";
+
+    Cache.removeFromCache(LAST_SEARCH_RESULT_CACHE_KEY);
 
     if (answer.Response === "True") {
-        numResults.textContent = `(${answer.totalResults} results found)`;
+        Cache.storeInCache(LAST_SEARCH_RESULT_CACHE_KEY, answer);
 
-        answer.Search.forEach(result => {
-            const h4 = document.createElement("h4");
-            const a = document.createElement("a");
-            a.href = "details.html?id=" + result.imdbID;
-            a.textContent = result.Title + " (" + result.Year + ")";
-            h4.appendChild(a);
-            h4.appendChild(getFavoriteElement(result.imdbID));
-            searchResults.appendChild(h4);
-        });
+        renderSearchResultAnswer(answer);
     } else if (answer.Error) {
         const error = document.createElement("h4");
         error.textContent = answer.Error;
-        searchResults.appendChild(error);
+        getE("#search-results").appendChild(error);
+    }
+}
+
+function renderSearchResultAnswer(answer) {
+    getE("#num-results").textContent = `(${answer.totalResults} results found)`;
+
+    answer.Search.forEach(result => {
+        const h4 = document.createElement("h4");
+        const a = document.createElement("a");
+        a.href = "details.html?id=" + result.imdbID;
+        a.textContent = result.Title + " (" + result.Year + ")";
+        h4.appendChild(a);
+        h4.appendChild(getFavoriteElement(result.imdbID));
+        getE("#search-results").appendChild(h4);
+    });
+}
+
+function restoreSearchFromCache() {
+    const answer = Cache.retrieveFromCache(LAST_SEARCH_RESULT_CACHE_KEY);
+
+    if (answer.SearchQuery) {
+        document.forms.search.query.value = answer.SearchQuery.query;
+        document.forms.search.year.value = answer.SearchQuery.year;
+        document.forms.search.type.value = answer.SearchQuery.type;
+    }
+
+    if (answer.Response === "True") {
+        getE("#search-results").textContent = "";
+        renderSearchResultAnswer(answer);
     }
 }
