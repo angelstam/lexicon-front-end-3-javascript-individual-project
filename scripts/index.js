93294f217b81e1f9c6c8a938bd201cf0874a6a06
Johan Angelstam <johan@angelstam.se>
2024-08-16 12:39:30 +0200

    Add loading of more results.

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,7 @@ const LAST_SEARCH_RESULT_CACHE_KEY = "last-search-result";
 
 window.addEventListener("load", () => {
     setupSearchListener();
+    setupLoadMoreSearchResultsListener();
 
     if (Cache.existsInCache(LAST_SEARCH_RESULT_CACHE_KEY)) {
         restoreSearchFromCache();
@@ -57,6 +58,7 @@ async function renderSearchResults(search) {
         Cache.storeInCache(LAST_SEARCH_RESULT_CACHE_KEY, answer);
 
         renderSearchResultAnswer(answer);
+        updateMoreSearchResultsElement();
     } else if (answer.Error) {
         const error = document.createElement("h4");
         error.textContent = answer.Error;
@@ -90,5 +92,58 @@ function restoreSearchFromCache() {
     if (answer.Response === "True") {
         getE("#search-results").textContent = "";
         renderSearchResultAnswer(answer);
+    }
+}
+
+function setupLoadMoreSearchResultsListener() {
+    updateMoreSearchResultsElement();
+
+    getE("#more-results").addEventListener("click", async event => {
+        // Load another page of the results.
+        if (hasMoreSearchResultsToLoad()) {
+            const lastAnswer = Cache.retrieveFromCache(LAST_SEARCH_RESULT_CACHE_KEY);
+            const search = lastAnswer.SearchQuery;
+
+            // Increment page
+            if (!search.page) {
+                search.page = 1;
+            }
+            search.page++;
+
+            const answer = await OMDb.searchOmdbMovie(search.query, search.type, search.year, search.page);
+
+            if (answer.Response === "True") {
+                // Update the cached search result.
+                lastAnswer.SearchQuery.page = search.page;
+                lastAnswer.totalResults = answer.totalResults;
+                lastAnswer.Search = lastAnswer.Search.concat(answer.Search);
+                Cache.storeInCache(LAST_SEARCH_RESULT_CACHE_KEY, lastAnswer);
+
+                renderSearchResultAnswer(answer);
+                updateMoreSearchResultsElement();
+            }
+        }
+    });
+}
+
+function hasMoreSearchResultsToLoad() {
+    const answer = Cache.retrieveFromCache(LAST_SEARCH_RESULT_CACHE_KEY);
+
+    if (answer && answer.totalResults > 10) {
+        if (!answer.SearchQuery.page) {
+            return true;
+        }
+        if (answer.Search.length < answer.totalResults) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function updateMoreSearchResultsElement() {
+    if (hasMoreSearchResultsToLoad()) {
+        getE("#more-results").style.display = "inline";
+    } else {
+        getE("#more-results").style.display = "none";
     }
 }
